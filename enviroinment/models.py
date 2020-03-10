35c151fa63169e11136f031525c004954c401fd0
Eduardo Machado <eduardo.machado@resale.com.br>
2020-03-10 18:31:16 -0300

    feat(enviroinment): create Enviroinment and Project models

--- enviroinment/models.py
+++ enviroinment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Enviroinment(BaseModel):
+    name = models.CharField(max_length=128, verbose_name="Nome do ambiente")
+    ip = models.GenericIPAddressField()
+
+
+class Project(BaseModel):
+    name = models.CharField(max_length=128)
+    enviroinment = models.ForeignKey(Enviroinment, on_delete=models.CASCADE)
+    url = models.URLField()
