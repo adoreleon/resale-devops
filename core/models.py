cd12ed74f8a6c73e385bb758059204178fb283ec
Eduardo Machado <eduardo.machado@resale.com.br>
2020-03-10 18:30:46 -0300

    feat(core): create BaseModel with default fields

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    updated_by = models.CharField(max_length=128)
