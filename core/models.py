34216f910b37ccc0f161048d898f27aab2911a50
Eduardo Machado <adoreleon@gmail.com>
2020-03-18 00:35:46 -0300

    feat(core): saves current logged user on updated_by field

--- core/models.py
+++ core/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import AnonymousUser
 from django.db import models
+
+from core.middleware import get_request
 
 
 class BaseModel(models.Model):
@@ -8,3 +11,13 @@ class BaseModel(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
     updated_by = models.CharField(max_length=128)
+
+    def save(self, *args, **kwargs):
+        self.updated_by = str(AnonymousUser)
+        current_request = get_request()
+        current_user = current_request.user
+
+        if current_request.user is not None:
+            self.updated_by = current_user
+
+        return super(BaseModel, self).save(*args, **kwargs)
