9ad7214ec160ec95c389a198e240b66d3aa5c864
Eduardo Machado <adoreleon@gmail.com>
2020-03-18 00:31:58 -0300

    feat(core): creates base admin class with readonly updated_by field

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class BaseModelAdmin(admin.ModelAdmin):
+    def __init__(self, *args, **kwargs):
+        if self.readonly_fields is not None and isinstance(self.readonly_fields, (list, set, tuple)):
+            self.readonly_fields += ("updated_by", )
+        super(BaseModelAdmin, self).__init__(*args, **kwargs)
--- enviroinment/admin.py
+++ enviroinment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.admin import BaseModelAdmin
+from enviroinment.models import Enviroinment, Project
+
+admin.site.register(Enviroinment, BaseModelAdmin)
+admin.site.register(Project, BaseModelAdmin)
