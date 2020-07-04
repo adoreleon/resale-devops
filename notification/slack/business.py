7817c4fe2c8f69735b2e829f8090ea84c9f1e1ff
rafaelmilanez <rafael.milanez@resale.com>
2020-07-04 17:24:17 -0300

    feat(slack_business): create messages and add bot token/channel name

--- notification/slack/business.py
+++ notification/slack/business.py
@@ -1,10 +1,16 @@
+from slack import WebClient
 from update_request.constants import EnumUpdateRequestStatus
 
 
 class SlackNotification:
+
+    bot_token = "xoxb-517055287185-843238946631-j0MzcWPXaOTbUo5AoPKjifpW"
+    channel = "#dev-alerts"
+
     update_request = None
 
     def __init__(self, update_request):
+        self.client = WebClient(token=self.bot_token)
         self.update_request = update_request
 
     def process_signal(self, state, status):
@@ -15,7 +21,23 @@ class SlackNotification:
             self.send_finished_notification()
 
     def send_pending_notification(self):
-        pass
+        user = self.update_request.user
+
+        message = "O usuário {user} solicitou a atualização do ambiente Y".format(user=user)
+
+        self.send_notification(message)
 
     def send_finished_notification(self):
-        pass
+        user = self.update_request.user
+
+        message = "O usuário {user} atualizou o ambiente Y".format(user=user)
+
+        self.send_notification(message)
+
+    def send_notification(self, message):
+        response = self.client.chat_postMessage(
+            channel=self.channel,
+            message=message
+        )
+
+        return response
